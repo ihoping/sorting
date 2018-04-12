ebddadaa88e2a03f8d5a669fd136e4c54e105545
lutiansong <new.gooltsgle@gmail.com>
2018-04-12 08:45:33 +0800

    更规范

--- sorts.php
+++ sorts.php
@@ -1,20 +1,41 @@
 <?php
+
 /**
- * Created by PhpStorm.
- * User: syw
- * Date: 17-5-5
- * Time: 下午4:28
- * Title:每天一个排序算法
+ * @param $a
+ * @param $b
  */
+function swap(&$a, &$b)
+{
+    $tmp = $a;
+    $a = $b;
+    $b = $tmp;
+}
+
+/**
+ * 冒泡排序
+ * @param array $arr
+ * @return array
+ */
+function bubble_sort(array $arr): array
+{
+    $len = count($arr);
+    for ($i = 0; $i < $len - 1; $i++) {
+        for ($j = 0; $j < $len - 1 - $i; $j++) {
+            if ($arr[$j] > $arr[$j + 1]) swap($arr[$j], $arr[$j + 1]);
+        }
+    }
+    return $arr;
+}
 
 /**
  * 插入排序
  * @param array $arr
  * @return array
  */
-function insert_sort(array $arr)
+function insert_sort(array $arr): array
 {
-    for ($i = 1; $i < count($arr); $i++) {
+    $len = count($arr);
+    for ($i = 1; $i < $len; $i++) {
         $key = $arr[$i];
         for ($j = $i - 1; $j >= 0 && $arr[$j] > $key; $j--) {
             $arr[$j + 1] = $arr[$j];
@@ -22,45 +43,23 @@ function insert_sort(array $arr)
         }
     }
     return $arr;
+
 }
 
 /**
- * 希尔排序 标准
- *
+ * 插入排序的改进-希尔排序
  * @param array $arr
  * @return array
  */
-function shell_sort(array $arr)
+function shell_sort(array $arr) : array
 {
-    $n = count($arr);
-    $step = 2;
-    $gap = intval($n / $step);
-    while ($gap > 0) {
-        for ($gi = 0; $gi < $gap; $gi++) {
-            for ($i = $gi; $i < $n; $i += $gap) {
-                $key = $arr[$i];
-                for ($j = $i - $gap; $j >= 0 && $arr[$j] > $key; $j -= $gap) {
-                    $arr[$j + $gap] = $arr[$j];
-                    $arr[$j] = $key;
-                }
-            }
-        }
-        $gap = intval($gap / $step);
-    }
-    return $arr;
-}
-function shell_sort1($arr)
-{
-    //计算数组长度
-    $length = count($arr);
-    //计算增量
-    for ($gap = $length >> 1; $gap > 0; $gap >>= 1) {
-        //根据增量进行分组，进行直接插入排序
-        for ($i = $gap; $i < $length; $i++) {
-            $tmp = $arr[$i];
-            for ($j = $i - $gap; $j > 0 && $tmp < $arr[$j]; $j -= $gap) {
+    $len = count($arr);
+    for ($gap = $len >> 1; $gap > 0; $gap >>= 1) {
+        for ($i = $gap; $i < $len; $i++) {
+            $key = $arr[$i];
+            for ($j = $i - $gap; $j >= 0 && $key < $arr[$j]; $j -= $gap) {
                 $arr[$j + $gap] = $arr[$j];
-                $arr[$j] = $tmp;
+                $arr[$j] = $key;
             }
         }
     }
@@ -69,52 +68,140 @@ function shell_sort1($arr)
 
 
 /**
- * 冒泡排序
- *
+ * 选择排序
  * @param array $arr
  * @return array
  */
-function bubble_sort($arr)
+function select_sort(array $arr) : array
 {
-    $count = count($arr);
-    //外层控制排序轮次
-    for ($i = 0; $i < $count - 1; $i++) {
-        //内层控制每轮比较次数
-        for ($j = 0; $j < $count - 1 - $i; $j++) {
-            if ($arr[$j] > $arr[$j + 1]) {
-                $temp = $arr[$j];
-                $arr[$j] = $arr[$j + 1];
-                $arr[$j + 1] = $temp;
-            }
+    $len = count($arr);
+    for ($i = 0; $i < $len - 1; $i++) {
+        $key = $i;
+        for ($j = $i + 1; $j < $len; $j++) {
+            if ($arr[$j] < $arr[$key]) $key = $j;
         }
+        swap($arr[$i], $arr[$key]);
     }
     return $arr;
 }
 
+/**
+ * 选择排序的改进--堆排序
+ * @param array $arr
+ * @return array
+ */
+function heap_sort(array $arr) : array
+{
+    $len = count($arr);
+    build_heap($arr);//第一次构建堆
+    while (--$len) {
+        swap($arr[0], $arr[$len]);//交换首尾
+        heap_adjust($arr, 0, $len);//重新调整堆
+    }
+    return $arr;
+}
+
+/**
+ * 第一次构建堆
+ * @param array $arr
+ */
+function build_heap(array &$arr)
+{
+    $len = count($arr);
+    for ($i = floor($len / 2) - 1; $i >= 0; $i--) {
+        heap_adjust($arr, $i, $len);
+    }
+}
+
+/**
+ * 调整堆
+ * @param array $arr
+ * @param int $i
+ * @param int $num
+ */
+function heap_adjust(array &$arr, $i, $num)
+{
+    if ($i > $num / 2) return;
+    $key = $i;
+    $left_child = 2 * $i + 1;
+    $right_child = 2 * $i + 2;
+    if ($left_child < $num && $arr[$key] < $arr[$left_child]) {
+        $key = $left_child;
+    }
+    if ($right_child < $num && $arr[$key] < $arr[$right_child]) {
+        $key = $right_child;
+    }
+    if ($key != $i) {
+        swap($arr[$i], $arr[$key]);
+        heap_adjust($arr, $key, $num);
+    }
+}
+
+/**
+ * 归并排序
+ * @param array $arr
+ * @return array
+ */
+function merge_sort(array $arr) : array
+{
+    $len = count($arr);
+    if ($len <= 1) return $arr;
+
+    $left = merge_sort(array_slice($arr, 0, floor($len / 2)));
+    $right = merge_sort(array_slice($arr, floor($len / 2)));
+    return merge($left, $right);
+}
+
+/**
+ * 归并排序合并函数
+ * @param array $left
+ * @param array $right
+ * @return array
+ */
+function merge(array $left, array $right) : array
+{
+    $res = [];
+    $i = $j = 0;
+    while ($i < count($left) && $j < count($right)) {
+        if ($left[$i] < $right[$j]) {
+            $res[] = $left[$i];
+            $i++;
+        } else {
+            $res[] = $right[$j];
+            $j++;
+        }
+    }
+
+    /*将剩下的放到后面*/
+    if ($i < count($left)) {
+        $res = array_merge($res, array_slice($left, $i));
+    }
+    if ($j < count($right)) {
+        $res = array_merge($res, array_slice($right, $j));
+    }
+
+    return $res;
+}
 
 /**
  * 快速排序
- *
  * @param array $arr
- * @param $left
- * @param $right
+ * @param int $left
+ * @param null $right
  * @return array
  */
-function quick_sort(array &$arr, $left = 0, $right = null)
+function quick_sort(array &$arr, $left = 0, $right = null) : array
 {
-    if (is_null($right)) {//是否为null，是就初始化
-        $right = count($arr) - 1;
-    }
-    if ($left >= $right) {//如果左下标大于等于右小标则说明排序完成，退出
-        return $arr;
-    }
-    $key = $arr[$left];//基准默认为left
-    //备份左右下标
+    $len = count($arr);
+    if (is_null($right)) $right = $len - 1;
+    if ($left >= $right) return $arr;
+
+    $key = $arr[$left];
     $low = $left;
     $high = $right;
 
     while ($left < $right) {
-        while ($left < $right && $arr[$right] > $key) {
+        while ($left < $right && $arr[$right] >= $key) {
             $right--;
         }
         $arr[$left] = $arr[$right];
@@ -123,132 +210,14 @@ function quick_sort(array &$arr, $left = 0, $right = null)
         }
         $arr[$right] = $arr[$left];
     }
-
     $arr[$right] = $key;
     quick_sort($arr, $low, $left - 1);
     quick_sort($arr, $left + 1, $high);
     return $arr;
 }
 
-/**
- * 直接选择排序
- *
- * @param array $arr
- * @return array
- */
-function select_sort(array $arr)
-{
-    $len = count($arr);
-    for ($i = 0; $i < $len - 1; $i++) {
-        $key = $i;
-        for ($j = $i + 1; $j < $len; $j++) {
-            if ($arr[$j] < $arr[$key]) {
-                $key = $j;
-            }
-        }
-        $val = $arr[$key];
-        $arr[$key] = $arr[$i];
-        $arr[$i] = $val;
-    }
-    return $arr;
-}
 
-/**
- * 堆排序
- *
- * @param array $arr
- * @return array
- */
-function heap_sort(array $arr)
-{
-    $len = count($arr);
-    build_heap($arr);//生成一个堆
-
-    while (--$len) {
-        swap($arr[0], $arr[$len]);
-        heap_adjust($arr, 0, $len);//重新调整堆
-    }
-    return $arr;
-}
-
-//第一次构建堆
-function build_heap(array &$arr)
-{
-    $len = count($arr);
-    for ($i = floor(($len) / 2) - 1; $i >= 0; $i--) {
-        heap_adjust($arr, $i, $len);
-    }
-}
-
-//调整堆
-function heap_adjust(array &$arr, $i, $num)
-{
-    if ($i > $num / 2) return;
-    $key = $i;
-    //因为从下标0开始，所以不是2*i和2*i + 1
-    $left_child = $i * 2 + 1;
-    $right_child = $i * 2 + 2;
-
-    if ($left_child < $num && $arr[$left_child] > $arr[$key]) {
-        $key = $left_child;
-    }
-
-    if ($right_child < $num && $arr[$right_child] > $arr[$key]) {
-        $key = $right_child;
-    }
-
-    if ($key != $i) {
-        swap($arr[$i], $arr[$key]);
-        heap_adjust($arr, $key, $num);
-    }
-}
-
-function swap(&$v1, &$v2)
-{
-    $tmp = $v1;
-    $v1 = $v2;
-    $v2 = $tmp;
-}
-
-
-/**
- * 归并排序
- *
- * @param array $arr
- * @return array
- */
-function merge_sort(array $arr)
-{
-    $n = count($arr);
-    if ($n <= 1) {
-        return $arr;
-    }
-    $left = merge_sort(array_slice($arr, 0, floor($n / 2)));
-    $right = merge_sort(array_slice($arr, floor($n / 2)));
-    $arr = merge($left, $right);
-    return $arr;
-}
-
-function merge(array $left, array $right)
-{
-    $arr = [];
-    $i = $j = 0;
-    while ($i < count($left) && $j < count($right)) {
-        if ($left[$i] < $right[$j]) {
-            $arr[] = $left[$i];
-            $i++;
-        } else {
-            $arr[] = $right[$j];
-            $j++;
-        }
-    }
-    $arr = array_merge($arr, array_slice($left, $i));
-    $arr = array_merge($arr, array_slice($right, $j));
-    return $arr;
-}
-
-var_dump(merge_sort([13, 2, 89, 10, 56]));exit();
-
+###########################下面的排序未测###########################
 /**
  * 基数排序
  *
